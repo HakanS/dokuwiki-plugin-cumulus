2569748123e82dfac3f703b7e23bb7bfe10f4cf8
Hakan Sandell <sandell.hakan@gmail.com>
2011-09-02 17:54:27 +0200

    French translation by Stéphane LAFORET

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * French language file for culumus plugin
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  laflaf3d <laflaf3d@yahoo.fr>
+ */
+ 
+$lang['filenotfound'] = '<div>--- Le fichier <strong>tagcloud.swf</strong> est introuvable ---</div>';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/fr/settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * French language file for culumus plugin
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  laflaf3d <laflaf3d@yahoo.fr>
+ */
+ 
+// for the configuration manager
+$lang['max']     = 'Nombre maximum de mots affichés';
+$lang['width']   = 'Largeur en pixels (500 ou plus est recommandé)';
+$lang['height']  = 'Hauteur en pixels (idéallement autour de 3/4 de la largeur)';
+$lang['tcolor']  = 'Couleur des tags (valeur HEXA sans le prefixe ‘#’)';
+$lang['tcolor2'] = 'Couleur optionnelle pour le gradient (valeur HEXA sans le prefixe ‘#’)';
+$lang['hicolor'] = 'Couleur optionnelle de surbrillance (valeur HEXA sans le prefixe ‘#’)';
+$lang['bgcolor'] = 'Couleur de fond (valeur HEXA sans le prefixe ‘#’)';
+$lang['trans']   = 'Transparence du fond';
+$lang['distr']   = 'Distribuer équitablement les balises sur la sphère';
+$lang['speed']   = 'Vitesse de rotation (percentage, default is 100)';
+$lang['showtags'] = 'Afficher les tags quand Flash n’est pas chargé';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
